5baad680570ea2dc0ba9390dae40806466c5accd
Amanda Simões <amandasimoesdev@gmail.com>
2023-06-24 15:05:59 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function tocaSomPom() {
+    document.querySelector ('#som_tecla_pom').play()
+}
+
+
+
+
+
+// document.querySelector ('#som_tecla_clap').play()
+// document.querySelector ('#som_tecla_tim').play()
+
+// document.querySelector('#som_tecla_puff')
+// document.querySelector('#som_tecla_splash')
+// document.querySelector('#som_toim')
+
+// document.querySelector('#som_tecla_psh')
+// document.querySelector('#som_tecla_tic')
+// document.querySelector('#som_tecla_tom')
+
