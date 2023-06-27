b85a9172ea7efcc4332104bc884657ced1513fd4
Amanda Simões <amandasimoesdev@gmail.com>
2023-06-26 22:42:51 -0300

    Projeto finalizado

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-function tocaSomPom() {
-    document.querySelector ('#som_tecla_pom').play()
+function tocaSom (IdElementAudio) {
+    document.querySelector (IdElementAudio).play()    
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+let  i = 0
+
+while (i < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[i];
+    const intrumento = tecla.classList[1]
+    const idAudio = `#som_${intrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    };
+
+    i++
 }
 
 
 
+/*
+Essa função abaixo funciona muito bem, mas como são 9 botões,
+precisaria fazer 9 funções diferentes para que cada botão funcione.
+Isso faria com que o codigo ficasse muito grande e sujeito a erros, 
+o que não é funcional.
+*/
 
+// function tocaSomPom() {
+//     document.querySelector ('#som_tecla_pom').play()
+// }
 
-// document.querySelector ('#som_tecla_clap').play()
-// document.querySelector ('#som_tecla_tim').play()
-
-// document.querySelector('#som_tecla_puff')
-// document.querySelector('#som_tecla_splash')
-// document.querySelector('#som_toim')
-
-// document.querySelector('#som_tecla_psh')
-// document.querySelector('#som_tecla_tic')
-// document.querySelector('#som_tecla_tom')
-
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
